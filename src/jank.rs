2b550c71b7bf9aabec250fe2eb8bf4c71bbfa971
Brady McDonough <brady.mcd.codes@gmail.com>
2020-02-18 19:02:01 -0700

    Added guards against instability. If JankIter structures are different from standards the build will fail.

--- src/jank.rs
+++ src/jank.rs
@@ -13,3 +13,35 @@ pub struct JankIterMut< 'a, T > {
     pub _marker: PhantomData< &'a mut T >,
 }
 
+#[cfg(test)]
+mod tests {
+
+    use crate::jank::{JankIter, JankIterMut};
+
+    use core::mem::{transmute, size_of};
+    use core::marker::PhantomData;
+
+    #[test]
+    fn test_jank( ) {
+        assert_eq!( size_of::<JankIter<u8>>(), size_of::<core::slice::Iter<u8>>());
+        assert_eq!( size_of::<JankIterMut<u8>>(), size_of::<core::slice::IterMut<u8>>());
+
+        let buffer: Vec< usize > = vec!( 1,2,3,4 );
+        let standard_iter = buffer.iter( );
+
+        let len = buffer.len( );
+        let ptr: *const usize = unsafe{ core::mem::transmute::< &Vec<usize>, &usize >( &buffer ) };
+        let end = unsafe{ ptr.add( len ) };
+        let jank_iter = JankIter {
+            ptr: ptr,
+            end: end,
+            _marker: PhantomData,
+        };
+        unsafe{
+            assert_eq!(
+                *transmute::< &JankIter<usize>, &[*const usize;2]>( &jank_iter ),
+                *transmute::< &core::slice::Iter<usize>, &[*const usize; 2]>( &standard_iter )
+            )
+        }
+    }
+}
